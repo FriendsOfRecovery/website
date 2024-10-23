1ea3e54ff787c21956eef3202128a9936a76cc91
FriendsOfRecovery <nathan@friendsofrecovery.com>
2024-10-23 15:39:06 -0500

    added hero banner

--- public/script.js
+++ public/script.js
@@ -2,3 +2,49 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('show');
 }
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Auto slideshow feature (optional)
+let autoSlideIndex = 0;
+autoShowSlides();
+
+function autoShowSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    autoSlideIndex++;
+    if (autoSlideIndex > slides.length) { autoSlideIndex = 1 }
+    slides[autoSlideIndex - 1].style.display = "block";
+    dots[autoSlideIndex - 1].className += " active";
+    setTimeout(autoShowSlides, 5000); // Change slide every 5 seconds
+}
